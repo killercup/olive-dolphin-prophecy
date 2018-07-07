f48f15ec29dfeba6e74579b5556ec822fdc42247
Pascal Hertleif <killercup@gmail.com>
2018-07-07 17:47:33 +0200

    Add more variants

--- app/src/main/java/de/hertleif/olivedolphinprophecy/Outcome.kt
+++ app/src/main/java/de/hertleif/olivedolphinprophecy/Outcome.kt
@@ -6,9 +6,9 @@ enum class Outcome {
 
     fun texts(): Set<String> {
         return if (this == Outcome.Yes) {
-            setOf("Yes", "Yep", "Sure", "Sounds good", "Go ahead", "Certainly", "Yes please", "Oh yes!", "Very well", "Absolutely", "Affirmative")
+            setOf("Yes", "Yep", "Sure", "Sounds good", "Go ahead", "Certainly", "Yes please", "Oh yes!", "Very well", "Absolutely", "Affirmative", "Alright", "Very well", "By all means", "Indeed", "Okey-dokey", "Aye aye!", "Fine", "Gladly", "Naturally", "Sure thing", "Totally")
         } else {
-            setOf("No", "Nope", "Nay", "Meh", "Never", "Oh god no", "Hell no", "Stop", "Most certainly not", "Not at all", "No way", "Negative")
+            setOf("No", "Nope", "Nay", "Meh", "Never", "Oh god no", "Hell no", "Stop", "Most certainly not", "Not at all", "No way", "Negative", "Under no circumstances", "Uh-uh", "by no means", "No thanks", "Out of the question", "Not for all the tea in China", "Not in a million years")
         }
     }
 }
